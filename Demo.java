05c3ffe4ebbf45b6a085a8234f0a5a7fc5197361
LAPTOP-B1PN513I\hp <kayum.ascorp@gmail.com>
2022-03-06 13:04:41 +0530

    changes

--- Demo.java
+++ Demo.java
@@ -1,6 +1,7 @@
 public class Demo
 {
 	public static void main(String[] args){
+		int  a=100;
 	   System.out.println("Github testing program");
 	}
 }
--- Demo2.java
+++ Demo2.java
@@ -1,6 +1,7 @@
 public class Demo2
 {
 	public void m1(){
-	
+	   int  x=1000;
+	   String name="raja";
 	}
 }
